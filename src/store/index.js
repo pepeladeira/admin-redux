fb4e4b2db35b15a391a5a01f0ab9861880eb61fc
Pedro Ladeira <pmladeira@live.com>
2020-08-19 14:48:50 -0300

    done

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,10 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import reducers from "./ducks";
-import showAlert from "./middlewares/showAlert";
 
 export default function () {
   return configureStore({
     reducers,
-    middleware: [...getDefaultMiddleware(), showAlert],
+    middleware: [...getDefaultMiddleware()],
   });
 }
